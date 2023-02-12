3274989c0156c06736774222b4068592f4afe6db
Muhammad-Riyead-Rahman <helloworld.asset@gmail.com>
2023-02-12 22:21:46 +0600

    finish

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import "./index.css";
 function App() {
   return <div>
     <h1 className='headingStyle largeText'>Todo App</h1>
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    <Card titleText="Call Mother" descText="This is desc1" />
+    <Card titleText="Call Father" descText="This is desc2" />
+    <Card titleText="Call Sister" descText="This is desc3" />
+    <Card titleText="call Home" descText="This is desc4" />
   </div>
 }
 
 export default App;
-
