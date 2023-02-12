2095d8cbd47b03809ac69b313f37309f4b7d9f4e
Muhammad-Riyead-Rahman <helloworld.asset@gmail.com>
2023-02-12 22:09:45 +0600

    finish

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// if create style sheet into src folder, must import it before use
+// & create style sheet into public folder, just call it by className
+// no need to import
 
 const todoTitte = "Call Family";
-const todoDesc = "Lorem, ipsum dolor sit amet consectetur adipisicing elit"
+const todoDesc = "This is todo description";
 const date = new Date();
 const dateName = date.getDate();
 const monthName = date.getMonth();
 const currentYear = date.getFullYear();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    <h1>Todo App</h1>
+    <h1 className='headingStyle largeText'>Todo App</h1>
     <h3>{todoTitte}</h3>
     <p>{todoDesc}</p>
     <p>{dateName + "/" + monthName + "/" + currentYear}</p>
