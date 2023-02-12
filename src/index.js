a56cd17d39e4f47eeff7b2b37ddef1479a32818b
Muhammad-Riyead-Rahman <helloworld.asset@gmail.com>
2023-02-12 22:16:47 +0600

    finish

--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
-import "./index.css";
-
-const todoTitte = "Call Family";
-const todoDesc = "Lorem, ipsum dolor sit amet consectetur adipisicing elit";
-const date = new Date();
-const dateName = date.getDate();
-const monthName = date.getMonth();
-const currentYear = date.getFullYear();
+import App from "./App.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <div>
-    <h1 className='headingStyle largeText'>Todo App</h1>
-    <div className='card'>
-      <h3 className='cardTitle'>{todoTitte}</h3>
-      <p className='cardDesc'>{todoDesc}</p>
-      <p className='cardFooter'>
-        {dateName + "/" + monthName + "/" + currentYear}
-      </p>
-    </div>
-  </div>
-);
+root.render(<App />);
