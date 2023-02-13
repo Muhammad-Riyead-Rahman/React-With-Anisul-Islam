791b54d1cfe83411761b34913508ba27e8184e89
Muhammad-Riyead-Rahman <helloworld.asset@gmail.com>
2023-02-13 17:16:04 +0600

    finish

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from './pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Navbar from './Navbar/navbar';
 import Blogs from './pages/Blogs';
 import Contact from './pages/Contact';
 import Error from './pages/Error';
+import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/blogs" element={<Blogs />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>This is Home Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe perferendis deleniti alias repellendus illum incidunt, optio quae tempore quo eos ducimus animi tempora amet autem aperiam, distinctio nesciunt fugit fugiat?</p>
+    </div>
+  )
+}
+
+export default Home
