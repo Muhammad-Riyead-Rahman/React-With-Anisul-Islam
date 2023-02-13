4aa118d402398ae96bd68990d27f0d94e7e24e3e
Muhammad-Riyead-Rahman <helloworld.asset@gmail.com>
2023-02-13 16:04:29 +0600

    finish

--- src/App.js
+++ src/App.js
@@ -1,108 +1,55 @@
-import { v4 as uuidv4 } from 'uuid';
-import Card from "./components/card";
-import data from "./data.json";
-import "./index.css";
+import React from 'react';
 
-
-const todos = [
-  {
-    id: uuidv4(),
-    title: "todo1",
-    desc: "todo1 description",
-    phones: [
-      { home: "0172564552" },
-      { office: "2555558525" }
-    ]
-  },
-  {
-    id: uuidv4(),
-    title: "todo2",
-    desc: "todo2 description",
-    phones: [
-      { home: "0172564552" },
-      { office: "2555558525" }
-    ]
-  },
-  {
-    id: uuidv4(),
-    title: "todo3",
-    desc: "todo3 description",
-    phones: [
-      { home: "0172564552" },
-      { office: "2555558525" }
-    ]
-  },
-  {
-    id: uuidv4(),
-    title: "todo4",
-    desc: "todo4 description",
-    phones: [
-      { home: "0172564552" },
-      { office: "2555558525" }
-    ]
-  }
-]
-
-function App() {
-  console.log(data[0].title);
-  let items = [];
-
-  /* // using for loop
-  for (let x = 0; x < data.length; x++) {
-    items.push(<Card titleText={data[x].title} descText={data[x].desc} />)
-  } */
-
-  items = data.map((item, index) => <Card key={index} titleText={item.title} descText={item.desc} />)
-
+/* simple function creation scenario */
+function Todo() {
   return (
     <div>
-      <h1 className='headingStyle largeText'>Todo App</h1>
-
-      {/* <Card titleText={data[0].title} descText={data[0].desc} />
-    <Card titleText={data[1].title} descText={data[1].desc} />
-    <Card titleText={data[2].title} descText={data[2].desc} />
-    <Card titleText={data[3].title} descText={data[3].desc} /> */}
-
-      {items}
-
-      {todos.map((todo) => {
-        console.log(todo)
-        const { id, title, desc, phones } = todo;
-        return (
-          <div key={id}>
-            <h3>{title}</h3>
-            <p>{desc}</p>
-            {todo.phones.map((phone, index) => {
-              const { home, office } = phone;
-              return (
-                <div key={index}>
-                  <p>Home: {home}</p>
-                  <p>office: {office}</p>
-                </div>
-              )
-            })}
-          </div>
-        )
-      })}
-
-      {todos.map((todo) => {
-        console.log(todo)
-        const { id, title, desc, phones } = todo;
-        console.log(phones)
-        const [{ home }, { office }] = phones;
-        return (
-          <div key={id}>
-            <h3>{title}</h3>
-            <p>{desc}</p>
-            <p>home: {home}</p>
-            <p>office: {office}</p>
-          </div>
-        )
-      })}
-    </div>)
+      <p>Todo title</p>
+      <p>Todo desc</p>
+    </div>
+  )
 }
 
-export default App;
+/* creating react function under the hood */
+
+//first scenario
+function Welcome1() {
+  return <h1>This is welcome 1</h1>
+}
+
+//secend scenario
+/* creating react function under the hood */
+function Welcome2() {
+  return React.createElement("h1", {}, "This is welcome2");
+}
+
+//third scenario
+function Todo1() {
+  return (
+    <div>
+      <p>Todo Title1</p>
+      <p>Todo Description1</p>
+    </div>
+  )
+}
+
+//fourth scenario
+/* creating react function under the hood */
+function Todo2 () {
+  return React.createElement("div", {}, React.createElement("p", {}, "Todo Title2"),
+    React.createElement("p", {}, "Todo Description2"))
+}
+
+export default function App() {
+  return (
+    <div>
+      <Welcome1 />
+      <Welcome2 />
+      <Todo1 />
+      <Todo2 />
+    </div>
+  )
+}
 
 
 
