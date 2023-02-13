3e74feb2d782984443f085a97cc1fe38a7d0027a
Muhammad-Riyead-Rahman <helloworld.asset@gmail.com>
2023-02-13 18:41:40 +0600

    finish

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 
-const Blogs = () => {
+const Blog = () => {
   return (
     <div>
-      <h1>This is Blogs Page</h1>
+      <h1>This is Blog Page</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam similique, cumque animi libero odit tempore facilis quam est assumenda suscipit voluptatum pariatur, nulla vero consequuntur? Adipisci reprehenderit beatae asperiores!</p>
     </div>
   )
 }
 
-export default Blogs
+export default Blog;
