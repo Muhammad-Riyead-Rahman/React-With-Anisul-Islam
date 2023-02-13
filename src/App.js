eece4a590aed00bec1d4c13727d9f6af3084a0eb
Muhammad-Riyead-Rahman <helloworld.asset@gmail.com>
2023-02-13 17:09:16 +0600

    finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import Users from './components/PropTypes/Users'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+import Blogs from './pages/Blogs';
+import Contact from './pages/Contact';
+import Error from './pages/Error';
 
 function App() {
   return (
-    <div>
-      <Users />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
