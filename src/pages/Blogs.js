12e7ce6a9354af8373c66cf2e3cb86af381adf4e
Muhammad-Riyead-Rahman <helloworld.asset@gmail.com>
2023-02-13 18:49:53 +0600

    finish

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,20 +1,19 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-
-import { blogsData } from '../blogsdata';
+import { Link } from "react-router-dom";
+import { blogsData } from "../data";
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState(blogsData);
-  // console.log(blogs);
+  console.log(blogs);
+
 
   const truncateString = (str, num) => {
-    if (str.lenght > num) {
-      return str.slice(0, num) + "...";
+    if (str.length > num) {
+      return str.slice(0, num) + "..."
     } else {
       return str;
     }
   }
-
   return (
     <div>
       <h1>Blogs Page</h1>
@@ -22,12 +21,12 @@ const Blogs = () => {
         {blogs.map((blog) => {
           const { id, title, body } = blog;
           return (
-            <article key={ id }>
-              <h3>{ title }</h3>
-              <p>{truncateString(body, 500)}</p>
-              <Link to={title} state={{ id, title, body }}>Learn More</Link>
+            <article key={id}>
+              <h3>{title}</h3>
+              <p>{truncateString(body, 100)}</p>
+              <Link to={title} state={{id, title, body}}>Learn More</Link>
             </article>
-            )
+          )
         })}
       </section>
     </div>
