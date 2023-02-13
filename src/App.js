e08b165d66a52a09597e9ab01e39a5add4b3e115
Muhammad-Riyead-Rahman <helloworld.asset@gmail.com>
2023-02-13 18:44:31 +0600

    finish

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Navbar from './Navbar/navbar';
 import Blog from './pages/Blog';
-import Blogs from './pages/Blogs';
-import BlogView from './pages/BlogView';
 import Contact from './pages/Contact';
 import Error from './pages/Error';
 import Home from './pages/Home';
+import Blogs from './pages/Blogs';
+import BlogView from './pages/BlogView';
+import User from './User';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/blog" element={<Blog />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/blogs/:title" element={<BlogView />} />
+        <Route path="/user" element={<User />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
