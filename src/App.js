b31790b07cc326db288594b8adf6cc4ba57b018e
Muhammad-Riyead-Rahman <helloworld.asset@gmail.com>
2023-02-13 16:10:50 +0600

    finish

--- src/App.js
+++ src/App.js
@@ -1,55 +1,14 @@
-import React from 'react';
+import ReactIcon from "./components/ReactIcon";
 
-/* simple function creation scenario */
-function Todo() {
+function App() {
   return (
     <div>
-      <p>Todo title</p>
-      <p>Todo desc</p>
+      <ReactIcon />
     </div>
   )
 }
 
-/* creating react function under the hood */
-
-//first scenario
-function Welcome1() {
-  return <h1>This is welcome 1</h1>
-}
-
-//secend scenario
-/* creating react function under the hood */
-function Welcome2() {
-  return React.createElement("h1", {}, "This is welcome2");
-}
-
-//third scenario
-function Todo1() {
-  return (
-    <div>
-      <p>Todo Title1</p>
-      <p>Todo Description1</p>
-    </div>
-  )
-}
-
-//fourth scenario
-/* creating react function under the hood */
-function Todo2 () {
-  return React.createElement("div", {}, React.createElement("p", {}, "Todo Title2"),
-    React.createElement("p", {}, "Todo Description2"))
-}
-
-export default function App() {
-  return (
-    <div>
-      <Welcome1 />
-      <Welcome2 />
-      <Todo1 />
-      <Todo2 />
-    </div>
-  )
-}
+export default App;
 
 
 
---
+++ src/components/ReactIcon.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { AiFillBug } from "react-icons/ai";
+
+const ReactIcon = () => {
+  return (
+        <AiFillBug />
+  );
+};
+
+export default ReactIcon;
