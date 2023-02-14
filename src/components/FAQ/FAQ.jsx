11e11798f26aa6a84358a6fbda3139980c635c94
Muhammad-Riyead-Rahman <helloworld.asset@gmail.com>
2023-02-14 09:01:33 +0600

    finish

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 
 import style from "./faq.module.css";
-const FAQ = ({ id, title, desc }) => {
+
+  const FAQ = ({ id, title, desc }) => {
   const [toggle, setToggle] = useState(false);
+    
   return (
     <article className={style.faq}>
       <div>
