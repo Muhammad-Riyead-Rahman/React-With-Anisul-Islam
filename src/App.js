c254524684f8ecae92483f432d765eb543090ff9
Muhammad-Riyead-Rahman <helloworld.asset@gmail.com>
2023-02-14 07:19:37 +0600

    finish

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
-import Card from "./components/card";
-import data from "./data.json";
 import "./index.css";
-
 
 const todos = [
   {
@@ -44,27 +41,9 @@ const todos = [
 ]
 
 function App() {
-  console.log(data[0].title);
-  let items = [];
-
-  /* // using for loop
-  for (let x = 0; x < data.length; x++) {
-    items.push(<Card titleText={data[x].title} descText={data[x].desc} />)
-  } */
-
-  items = data.map((item, index) => <Card key={index} titleText={item.title} descText={item.desc} />)
-
   return (
     <div>
       <h1 className='headingStyle largeText'>Todo App</h1>
-
-      {/* <Card titleText={data[0].title} descText={data[0].desc} />
-    <Card titleText={data[1].title} descText={data[1].desc} />
-    <Card titleText={data[2].title} descText={data[2].desc} />
-    <Card titleText={data[3].title} descText={data[3].desc} /> */}
-
-      {items}
-
       {todos.map((todo) => {
         console.log(todo)
         const { id, title, desc, phones } = todo;
