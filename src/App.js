4b318dbd4786c0b36fee93c0a6ccfd3b54a83cdb
Muhammad-Riyead-Rahman <hello.riyead@gmail.com>
2023-09-10 17:24:07 +0600

    n

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Note the import changes
+
 import Home from './pages/Home';
 import Blogs from './pages/Blogs';
 import Contact from './pages/Contact';
@@ -6,14 +7,14 @@ import Error from './pages/Error';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
+    <Router>
+      <Routes> {/* Use Routes instead of Route */}
         <Route path="/home" element={<Home />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<Error />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   )
 }
 
